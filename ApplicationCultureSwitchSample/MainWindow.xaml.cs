1186b36c9d5b18a5f5c1484f7294d5ff3c1eadf2
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-01-05 00:24:38 +0900

    Add ApplicationCultureSwitchSample project

---
+++ ApplicationCultureSwitchSample/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationCultureSwitchSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CultureInfoSwithButton_Click(object sender, RoutedEventArgs e)
+        {
+            var cultureStr = ((Button)sender).Tag.ToString()!;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureStr);
+
+            lblNumber.Content = (123456789.42d).ToString("N2");
+            lblDate.Content = DateTime.Now.ToString();
+        }
+    }
+}
