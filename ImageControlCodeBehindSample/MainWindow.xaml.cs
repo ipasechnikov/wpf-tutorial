28455bbb99358f2c670f31a49aa2b16723b1c75c
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-04-17 23:38:58 +0900

    Add ImageControlCodeBehindSample project

---
+++ ImageControlCodeBehindSample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+namespace ImageControlCodeBehindSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var fileUri = new Uri(openFileDialog.FileName);
+                imgDynamic.Source = new BitmapImage(fileUri);
+            }
+        }
+
+        private void btnLoadFromResource_Click(object sender, RoutedEventArgs e)
+        {
+            var resourceUri = new Uri("/Images/white_bengal_tiger.jpg", UriKind.Relative);
+            imgDynamic.Source = new BitmapImage(resourceUri);
+        }
+    }
+}
