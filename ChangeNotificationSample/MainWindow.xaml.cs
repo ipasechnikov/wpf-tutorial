b4517525965fec8d9d2238c454656bef76fc5fd8
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-05-06 18:57:12 +0900

    Add ChangeNotificationSample project

---
+++ ChangeNotificationSample/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeNotificationSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> _users = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _users.Add(new User() { Name = "John Doe" });
+            _users.Add(new User() { Name = "Jane Doe" });
+
+            lbUsers.ItemsSource = _users;
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            _users.Add(new User() { Name = "New user" });
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                ((User)lbUsers.SelectedItem).Name = "Random Name";
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                _users.Remove((User)lbUsers.SelectedItem);
+        }
+    }
+
+    public class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value)
+                    return;
+
+                _name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
