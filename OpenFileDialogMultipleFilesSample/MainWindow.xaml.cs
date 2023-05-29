4692dff73cb79506fedc1a09e5aba43759a035ee
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-05-29 22:30:52 +0900

    Add OpenFileDialogMultipleFilesSample project

---
+++ OpenFileDialogMultipleFilesSample/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+using Microsoft.Win32;
+
+namespace OpenFileDialogMultipleFilesSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+                foreach (var fileName in openFileDialog.FileNames)
+                    lbFiles.Items.Add(Path.GetFileName(fileName));
+        }
+    }
+}
