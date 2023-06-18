2eacd8f9dfa32a52d24e1501ca82f6e583ccf841
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-06-19 00:38:47 +0900

    Add StatusBarAdvancedSample project

---
+++ StatusBarAdvancedSample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StatusBarAdvancedSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+            var col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
+            lblCursorPosition.Text = $"Line {row + 1}, Char {col + 1}";
+        }
+    }
+}
